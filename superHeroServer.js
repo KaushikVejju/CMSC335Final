e3d9b111195756f6ec6b5e26ad02bd20fa3423c3
Kaushik <kaush2003@gmail.com>
2023-05-09 00:00:52 -0400

    working

--- superHeroServer.js
+++ superHeroServer.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require("path");
 const bodyParser = require("body-parser");
+const fetch=require("node-fetch");
 const app = express();
 app.set("views", path.resolve(__dirname, "templates"));
 app.use(bodyParser.urlencoded({extended:false}));
